013d17c65d89c53596e639a5fb092c879962e09c
maathangi96 <maathangi96@gmail.com>
2018-01-28 18:04:42 +0530

    Update and rename hol.java to holiday.java

---
+++ holiday.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class holiday
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int i=0;
+String a=sc.nextLine();
+String s[]={"Sunday","Saturday"};
+if(a.equals(s[i]))
+
+System.out.println("yes");
+
+else
+
+System.out.println("no");
+
+}
+}
