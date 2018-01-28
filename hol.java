7c587d34c5860bfee97bd1cb3665c0c98020253d
maathangi96 <maathangi96@gmail.com>
2018-01-28 17:52:32 +0530

    Create hol.java

---
+++ hol.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class hol
+{
+public static void mmain(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int i;
+String s[]={'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'};
+if(s[i]==(Saturday || Sunday))
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
